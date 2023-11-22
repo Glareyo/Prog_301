a1336e10021920491fc7877564c789efcefadf42
cedillo02 <cedillo02@VDIIAMIC4QM3-12>
2023-11-21 21:05:01 -0600

    Added Sprint 11 Demo

---
+++ Sprint11_Demo/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sprint11_Demo.Controllers
+{
+    public class TestController : Controller
+    {
+
+        public string IndexString()
+        {
+            return "Hello!";
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
